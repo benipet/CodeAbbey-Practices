b938cb3df269653381ca00f615331c45b93d8418
benipet <peter.benis2014@gmail.com>
2023-01-29 13:26:51 +0000

    Added new feature to the triangles code

--- Triangles/Triangles.cpp
+++ Triangles/Triangles.cpp
@@ -6,16 +6,40 @@
 */
 
 int isTriangle(int a, int b, int c);
+int input_triplets(void);
 
 int main(void) {
+	int N;
+	int i;
 	int a, b, c;
-	printf("Please enter the sides of the triangle\n");
-	scanf_s("%d %d %d", &a, &b, &c);
-	printf("%d", isTriangle(a, b, c));
+
+	N = input_triplets();
+
+	if (N > 0) {
+		for (i=1; i <= N; i++) {
+			printf("Please enter the sides of the triangle\n");
+			scanf_s("%d %d %d", &a, &b, &c);
+			printf("%d\n", isTriangle(a, b, c));
+
+		}
+		
+	}
+
+	
+	
 	
 	return 0;
 }
 
+int input_triplets(void) {
+	int N;
+
+	printf("Please enter how many triplets you want!\n");
+	scanf_s("%d", &N);
+
+	return N;
+
+}
 
 int isTriangle(int a, int b, int c) {
 	if (a + b < c)
